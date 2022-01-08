d779d590b71300946a076f5b5fe7b28121433fde
Suraj kumar <81457983+Suraj-Roy@users.noreply.github.com>
2022-01-08 15:02:34 +0530

    Update settings.py

--- Banking/settings.py
+++ Banking/settings.py
@@ -26,7 +26,7 @@ EMAIL_PORT = EMAIL_PORT
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-aj67)lec#2c!x$cfy*x^ios^30vxy035$c%9@q0ne01asz9yql'
+SECRET_KEY = 'SecretKey'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
